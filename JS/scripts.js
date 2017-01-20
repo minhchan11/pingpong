814e1dac960da9fa531a8c935bc2b5e314ebc6b0
Minh Phuong <mphuong@kent.edu>
2017-01-20 10:16:22 -0800

    add clear previous result texts to JS

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,16 +3,17 @@ var Input = 0;
 var result = 0;
 
 function generator () {
+  $("#result").text("");
   for (var i = 1; i <= Input; i++) {
     result = i.toString();
     if ( i % 15 === 0){
-        result = "ping-pong"
-      } else if (i % 3 === 0) {
-        result = "ping"
-      } else if (i % 5 === 0) {
-        result = "pong"
-      }
-      $("#result").append("<li>"+result+"</li>")
+      result = "ping-pong"
+    } else if (i % 3 === 0) {
+      result = "ping"
+    } else if (i % 5 === 0) {
+      result = "pong"
+    }
+    $("#result").append("<li>"+result+"</li>")
   };
 };
 
