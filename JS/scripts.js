0e1187d7eeb4cb38b58c9069365a4447e4b9c3af
Minh Phuong <mphuong@kent.edu>
2017-01-20 08:43:49 -0800

    define basic function for business logic

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,10 +1,18 @@
 //BUSINESS LOGIC
 var Input = 0;
+var result = 0;
+
+function generator () {
+  for (var i = 1; i <= Input; i++) {
+    console.log(i);
+  };
+};
 
 //USER INTERFACE
 $("document").ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     Input = parseInt($("#userinput").val());
+    generator(Input);
   });
 });
