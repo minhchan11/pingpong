fc2b5c2dee9ffde8fb62d7c6be4c79ec51e213cd
Minh Phuong <mphuong@kent.edu>
2017-01-20 09:31:05 -0800

    define replace function for business logic

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,10 +1,21 @@
 //BUSINESS LOGIC
-var Input = 0;
-var result = 0;
+var Input = 0;;
+var Output = []
 
 function generator () {
   for (var i = 1; i <= Input; i++) {
-    console.log(i);
+    Output.push(i);
+    for (var j = 0; j < Output.length; j++) {
+      if ( Output[j] % 15 === 0){
+        Output.splice(j,1,"ping-pong");
+      } else if (Output[j] % 3 === 0) {
+        Output.splice(j,1,"ping");
+      } else if (Output[j] % 5 === 0) {
+        Output.splice(j,1,"pong");
+      };
+      $("#result").append("<li>"+Output[j]+"</li>")
+    }
+
   };
 };
 
