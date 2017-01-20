7f15d94bf44536603d9e712050a62973b642596b
Minh Phuong <mphuong@kent.edu>
2017-01-20 08:34:40 -0800

    define var Input

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,8 +1,10 @@
 //BUSINESS LOGIC
+var Input = 0;
 
 //USER INTERFACE
 $("document").ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
+    Input = parseInt($("#userinput").val());
   });
 });
