415b56631cc64f98dbf79b7149f7a9509b9739c7
minhchan11 <phuong.nhat.minh.306@gmail.com>
2017-01-26 14:12:29 -0800

    got rid of global variables

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,14 +1,11 @@
 //BUSINESS LOGIC
-var Input = 0;
-var result = 0;
-
 function generator () {
   $("#result").text("");
   if (Input <= 0) {
     alert("Please put in positive integers")
   } else {
     for (var i = 1; i <= Input; i++) {
-      result = i.toString();
+      var result = i.toString();
       if ( i % 15 === 0){
         result = "ping-pong"
       } else if (i % 3 === 0) {
@@ -27,7 +24,7 @@ $("document").ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     $("#generated").slideUp(200);
-    Input = parseInt($("#userinput").val());
+    var Input = parseInt($("#userinput").val());
     $("#generated").fadeIn(2000);
     generator(Input);
 
